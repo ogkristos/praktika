018de2393b1b87899d97f9dd51be093f8ad3d069
Ramzikkk <ramazanbertysbayev@gmail.com>
2025-06-13 10:47:58 +0500

    Add build script with migrations and superuser creation

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
+python -c "
+from django.contrib.auth.models import User
+try:
+    User.objects.create_superuser('admin', 'admin@example.com', 'Admin123!')
+    print('Superuser created')
+except:
+    print('Superuser already exists')
+"
